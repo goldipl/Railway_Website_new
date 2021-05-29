6aae33167070308d4cbafc5bc2bcfe4895d3005d
goldipl <mgodlewski92@gmail.com>
2021-05-29 14:04:53 +0200

    delete body lock on mobile menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 const menuButton = document.querySelector(".menuButton");
 const menuItems = document.querySelector(".menuList");
 const expandListButton = document.querySelectorAll(".expandList");
-const bodyTag = document.querySelector("body");
+// const bodyTag = document.querySelector("body");
 
 // Opening and closing hamburger menu
 menuButton.addEventListener("click", () => {
     menuButton.classList.toggle("open");
     menuItems.classList.toggle("open");
-    bodyTag.classList.toggle("bodyLock");
+    // bodyTag.classList.toggle("bodyLock");
 });
 
 // Expanding mobile menu
